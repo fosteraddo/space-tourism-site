6b2a9fdbcc447243fe205ec610fbeafb6e1cf103
Foster Addo <addofoster047@gmail.com>
2023-04-12 06:49:43 -0700

    edit commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// document.querySelector(".test").setAttribute("src", "https://technology-vehicle.html");
+// document.querySelector(".circle").style.backgroundColor = "red";//This is really working.
+// var activeButton = document.querySelectorAll(".selected")
+// document.querySelector(".selected").classList.add("selected");
+// document.querySelector(".selected").classList.remove("selected")
+
+$("#technology-link a .selected").on("click", function(){
+    $(".technology-intro").text("hello, what up")
+
+})
